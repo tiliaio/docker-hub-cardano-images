cc6be4b2c47e03e42dd4995dd6d7f61af2dd3278
Mark Stopka <mark.stopka@perlur.cloud>
2020-07-19 11:40:43 +0200

    Refactor scripts 1.16.0 - HEALTHCHECK (#81)

--- cardano-node/1.16.0/usr/local/bin/docker-entrypoint.sh
+++ cardano-node/1.16.0/usr/local/bin/docker-entrypoint.sh
@@ -12,40 +12,40 @@ if [[ ! -f ${GENESIS_FILE} ]]; then
   echo 'Genesis file does not exist! 'cardano-node' can NOT start!!!'
   preExitHook "$@"
   exit
-elif [[ ! -f ${CARDANO_NODE_CONF_FILE} ]]; then
+elif [[ ! -f ${CNODE_CONF_FILE} ]]; then
   echo "'cardano-node' config file does not exists! 'cardano-node' can NOT start!!!"
   preExitHook "$@"
   exit
-elif [[ ! -f ${CARDANO_NODE_TOPOLOGY_FILE} ]]; then
+elif [[ ! -f ${CNODE_TOPOLOGY_FILE} ]]; then
   echo "'cardano-node' topology file does not exists! 'cardano-node' can NOT start!!!"
   preExitHook "$@"
   exit
-elif [[ ! -d ${CARDANO_NODE_DB_PATH} ]]; then
+elif [[ ! -d ${CNODE_DB_PATH} ]]; then
   echo "'cardano-node' database directory does not exists! 'cardano-node' can NOT start!!!"
   preExitHook "$@"
   exit
 fi
 
-if [[ ! -f ${CARDANO_NODE_SECRET_FILE} ]]; then
+if [[ ! -f ${CNODE_SECRET_FILE} ]]; then
   echo "'cardano-node' secret file not found! 'cardano-node' will start in a passive mode!"
-  echo "Config file: ${CARDANO_NODE_CONF_FILE}"
-  echo "Topology file: ${CARDANO_NODE_TOPOLOGY_FILE}"
-  echo "Database path: ${CARDANO_NODE_DB_PATH}"
-  echo "Socket path: ${CARDANO_NODE_SOCKET}"
-  echo "Node port: ${CARDANO_NODE_PORT}"
+  echo "Config file: ${CNODE_CONF_FILE}"
+  echo "Topology file: ${CNODE_TOPOLOGY_FILE}"
+  echo "Database path: ${CNODE_DB_PATH}"
+  echo "Socket path: ${CARDANO_NODE_SOCKET_PATH}"
+  echo "Node port: ${CNODE_PORT}"
   cardano-node run \
-    --config ${CARDANO_NODE_CONF_FILE} \
-    --topology ${CARDANO_NODE_TOPOLOGY_FILE} \
-    --database-path ${CARDANO_NODE_DB_PATH} \
+    --config ${CNODE_CONF_FILE} \
+    --topology ${CNODE_TOPOLOGY_FILE} \
+    --database-path ${CNODE_DB_PATH} \
     --socket-path ${CARDANO_NODE_SOCKET_PATH} \
-    --port ${CARDANO_NODE_PORT}
+    --port ${CNODE_PORT}
 
 else
   echo "'cardano-node' secret file found! 'cardano-node' will start in a slot leader mode!"
   cardano-node run \
-    --config ${CARDANO_NODE_CONF_FILE} \
-    --topology ${CARDANO_NODE_TOPOLOGY_FILE} \
-    --database-path ${CARDANO_NODE_DB_PATH} \
+    --config ${CNODE_CONF_FILE} \
+    --topology ${CNODE_TOPOLOGY_FILE} \
+    --database-path ${CNODE_DB_PATH} \
     --socket-path ${CARDANO_NODE_SOCKET_PATH} \
-    --port ${CARDANO_NODE_PORT}
+    --port ${CNODE_PORT}
 fi
--- cardano-node/1.16.0/usr/local/lib/cardano-node-set-env-variables.sh
+++ cardano-node/1.16.0/usr/local/lib/cardano-node-set-env-variables.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
-CARDANO_NODE_USER_HOME=$HOME
-if [ -f ${CARDANO_NODE_USER_HOME}etc/genesis.json ]; then
-  GENESIS_FILE=${CARDANO_NODE_USER_HOME}etc/genesis.json
+set -e
+
+CURRENT_TIME=$(date +%s)
+CONTAINER_START_TIME=$(stat --format %X /proc/1/)
+CONTAINER_UPTIME=$((${CURRENT_TIME} - ${CONTAINER_START_TIME}))
+
+CNODE_USER_HOME=$HOME
+if [ -f ${CNODE_USER_HOME}etc/genesis.json ]; then
+  GENESIS_FILE=${CNODE_USER_HOME}etc/genesis.json
 fi
 
-if [ -f ${CARDANO_NODE_USER_HOME}etc/topology.json ]; then
-  CARDANO_NODE_TOPOLOGY_FILE=${CARDANO_NODE_USER_HOME}etc/topology.json
+if [ -f ${CNODE_USER_HOME}etc/topology.json ]; then
+  CNODE_TOPOLOGY_FILE=${CNODE_USER_HOME}etc/topology.json
 fi
 
-if [ -f ${CARDANO_NODE_USER_HOME}etc/config.json ]; then
-  CARDANO_NODE_CONF_FILE=${CARDANO_NODE_USER_HOME}etc/config.json
-  CARDANO_NODE_CONF_TYPE="json"
-elif [ -f ${CARDANO_NODE_USER_HOME}etc/config.yaml ]; then
-  CARDANO_NODE_CONF_FILE=${CARDANO_NODE_USER_HOME}etc/config.yaml
-  CARDANO_NODE_CONF_TYPE="yaml"
+if [ -f ${CNODE_USER_HOME}etc/config.json ]; then
+  CNODE_CONF_FILE=${CNODE_USER_HOME}etc/config.json
+  CNODE_CONF_TYPE="json"
+elif [ -f ${CNODE_USER_HOME}etc/config.yaml ]; then
+  CNODE_CONF_FILE=${CNODE_USER_HOME}etc/config.yaml
+  CNODE_CONF_TYPE="yaml"
 fi
-if [ -f ${CARDANO_NODE_USER_HOME}etc/secrets/node_secret.yaml ]; then
-  CARDANO_NODE_SECRET_FILE=${CARDANO_NODE_USER_HOME}etc/secrets/node_secret.yaml
-  CARDANO_NODE_NODE_TYPE="pool"
-  CARDANO_NODE_POOL_ID=$(grep 'node_id' ${JORM_SECRET_FILE} | awk '{print $2}')
+if [[ -z ${CNODE_NODE_TYPE+x} ]]; then
+  if [ -f ${CNODE_USER_HOME}etc/secrets/node_secret.yaml ]; then
+    CNODE_SECRET_FILE=${CNODE_USER_HOME}etc/secrets/node_secret.yaml
+    CNODE_NODE_TYPE="leader"
+    CNODE_POOL_ID=$(grep 'node_id' ${JORM_SECRET_FILE} | awk '{print $2}')
+  else
+    CNODE_NODE_TYPE="relay"
+  fi
 else
-  CARDANO_NODE_NODE_TYPE="relay"
+  CNODE_NODE_TYPE=${CNODE_NODE_TYPE}
 fi
 
-: ${CARDANO_NODE_DB_PATH:=${CARDANO_NODE_DB_PATH:-${CARDANO_NODE_USER_HOME}/storage/}}
-: ${CARDANO_NODE_PORT:=${CARDANO_NODE_PORT:-9000}}
-: ${CARDANO_NODE_SOCKET_PATH:=${CARDANO_NODE_SOCKET_PATH:-/var/run/cardano/cardano-node.sock}}
+: ${CNODE_DB_PATH:=${CNODE_DB_PATH:-${CNODE_USER_HOME}/storage/}}
+: ${CNODE_PORT:=${CNODE_PORT:-9000}}
+: ${CNODE_SOCKET_PATH:=${CNODE_SOCKET_PATH:-/var/run/cardano/cardano-node.sock}}
 
-: ${CARDANO_NODE_MAX_STARTUP_TIME:=${CARDANO_NODE_MAX_STARTUP_TIME:-20}}
-: ${CARDANO_NODE_MAX_FAULT_UPTIME:=${CARDANO_NODE_MAX_FAULT_UPTIME:-600}}
+: ${CNODE_MAX_STARTUP_TIME:=${CNODE_MAX_STARTUP_TIME:-20}}
+: ${CNODE_MAX_FAULT_UPTIME:=${CNODENODE_MAX_FAULT_UPTIME:-600}}
 : ${POOLTOOL_MAX_BLOCK_DELTA:=${POOLTOOL_MAX_BLOCK_DELTA:-5}}
 
-export CARDANO_NODE_SOCKET_PATH=${CARDANO_NODE_SOCKET_PATH}
+export CARDANO_NODE_SOCKET_PATH=${CNODE_SOCKET_PATH}
+
+set +e
